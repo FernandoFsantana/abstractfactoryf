d6e55c4cfb973f4ba7d72ba236c5750655579d73
FernandoFsantana <35466498+FernandoFsantana@users.noreply.github.com>
2019-03-20 20:26:00 -0300

    Add files via upload

---
+++ factorymethod/FactoryMethod.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author fernando
+ */
+public class FactoryMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        FactoryVerificaFabricante celular = new FactoryVerificaFabricante();
+        String fabricante = "a";
+        String modelo = "IPhone";
+        celular.getFabricaCelular(fabricante, modelo);
+        
+    }
+    
+}
---
+++ factorymethod/FactoryVerificaFabricante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author fernando
+ */
+public class FactoryVerificaFabricante {
+    
+    public FabricaCelular getFabricaCelular(String fabricante, String modelo){
+     
+        if(fabricante.equals("a"))
+         return new ModeloCaro(modelo);
+        if(fabricante.equals("c"))
+         return new ModeloBarato(modelo);
+        
+        
+        return null;
+    }
+    
+}
